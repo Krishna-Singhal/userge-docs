1ab4bcc5717931ad17e2f718eab35e1a1d17d544
Krishna-Singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-05-24 11:21:11 +0530

    FIxed docs

--- javascripts/animation.js
+++ javascripts/animation.js
@@ -1,130 +1,210 @@
-version_txt = [
-    'import',
-    ' requests\n\n',
-    'return',
-    ' requests.get("',
-    'https://api.userge.tk/version',
-    '").json()'
-]
-getban_txt = [
-    'import',
-    ' requests\n\n',
-    'token ',
-    '=',
-    ' "your_api_key"\n',
-    'id', ' ', '=', ' ',  '777000',
-    '\nurl ', '=', ' ',
-    'f"https://api.userge.tk/getban?api_key={',
-    'token', '}&user_id={', 'id', '}', '"\n\n',
-    'return', ' requests.get(', 'url', ').json()'
-]
-getallban_txt = [
-    'import',
-    ' requests\n\n',
-    'token ',
-    '=',
-    ' "your_api_key"',
-    '\nurl ', '=', ' ',
-    'f"https://api.userge.tk/getban?api_key={',
-    'token', '}', '"\n\n',
-    'return', ' requests.get(', 'url', ').json()'
-]
-addban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
-'    "api_key": "your_api_key",\n',
-'    "user_id": ', '777000', ',\n',
-'    "reason": "Test reason"',
-'\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/addban', '", data', '=', 'data).json()']
-updateban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
-'    "api_key": "your_api_key",\n',
-'    "user_id": ', '777000', ',\n',
-'    "reason": "Test reason"',
-'\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/updateban', '", data', '=', 'data).json()']
-deleteban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
-'    "api_key": "your_api_key",\n',
-'    "user_id": ', '777000', ',',
-'\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/deleteban', '", data', '=', 'data).json()']
-create_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/createtoken', '"\n\ndata ', '=', ' {\n',
-'    "user_id": ', '777000', ',\n',
-'    "name": "Krishna"',
-'\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
-promote_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/promotetoken', '"\n\ndata ', '=', ' {\n',
-'    "api_key": "your_api_key"\n',
-'    "user_id": ', '777000', ',\n',
-'    "perms": "can_ban can_unban"',
-'\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
-demote_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/demotetoken', '"\n\ndata ', '=', ' {\n',
-'    "api_key": "your_api_key"\n',
-'    "user_id": ', '777000',
-'\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
-delete_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/deletetoken', '"\n\ndata ', '=', ' {\n',
-'    "api_key": "your_api_key"\n',
-'    "user_id": ', '777000',
-'\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
-stats_txt = [
-    'import',
-    ' requests\n\n',
-    'return',
-    ' requests.get("',
-    'https://api.userge.tk/stats',
-    '").json()'
-]
+// version_txt = [
+//     'import',
+//     ' requests\n\n',
+//     'return',
+//     ' requests.get("',
+//     'https://api.userge.tk/version',
+//     '").json()'
+// ]
+// getban_txt = [
+//     'import',
+//     ' requests\n\n',
+//     'token ',
+//     '=',
+//     ' "your_api_key"\n',
+//     'id', ' ', '=', ' ',  '777000',
+//     '\nurl ', '=', ' ',
+//     'f"https://api.userge.tk/getban?api_key={',
+//     'token', '}&user_id={', 'id', '}', '"\n\n',
+//     'return', ' requests.get(', 'url', ').json()'
+// ]
+// getallban_txt = [
+//     'import',
+//     ' requests\n\n',
+//     'token ',
+//     '=',
+//     ' "your_api_key"',
+//     '\nurl ', '=', ' ',
+//     'f"https://api.userge.tk/getban?api_key={',
+//     'token', '}', '"\n\n',
+//     'return', ' requests.get(', 'url', ').json()'
+// ]
+// addban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
+// '    "api_key": "your_api_key",\n',
+// '    "user_id": ', '777000', ',\n',
+// '    "reason": "Test reason"',
+// '\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/addban', '", data', '=', 'data).json()']
+// updateban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
+// '    "api_key": "your_api_key",\n',
+// '    "user_id": ', '777000', ',\n',
+// '    "reason": "Test reason"',
+// '\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/updateban', '", data', '=', 'data).json()']
+// deleteban_txt = ['import', ' requests\n\n', 'data ', '=', ' {\n',
+// '    "api_key": "your_api_key",\n',
+// '    "user_id": ', '777000', ',',
+// '\n}\n\n', 'return', ' requests.post("', 'https://api.userge.tk/deleteban', '", data', '=', 'data).json()']
+// create_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/createtoken', '"\n\ndata ', '=', ' {\n',
+// '    "user_id": ', '777000', ',\n',
+// '    "name": "Krishna"',
+// '\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
+// promote_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/promotetoken', '"\n\ndata ', '=', ' {\n',
+// '    "api_key": "your_api_key"\n',
+// '    "user_id": ', '777000', ',\n',
+// '    "perms": "can_ban can_unban"',
+// '\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
+// demote_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/demotetoken', '"\n\ndata ', '=', ' {\n',
+// '    "api_key": "your_api_key"\n',
+// '    "user_id": ', '777000',
+// '\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
+// delete_token = ['import', ' requests\n\n', 'url ', '=', ' "', 'https://api.userge.tk/deletetoken', '"\n\ndata ', '=', ' {\n',
+// '    "api_key": "your_api_key"\n',
+// '    "user_id": ', '777000',
+// '\n}\n\n', 'return', ' requests.post(', 'url', ', data', '=', 'data).json()']
+// stats_txt = [
+//     'import',
+//     ' requests\n\n',
+//     'return',
+//     ' requests.get("',
+//     'https://api.userge.tk/stats',
+//     '").json()'
+// ]
 
-//  yaha tak sab text h
+auth_text = `import userge_fed
 
-// carouselcheck = {
-//     "version": false,
-//     "getban": false,
-//     "getallbans": false,
-//     "addban": false,
-//     "updateban": false,
-//     "deleteban": false,
-//     "createtoken": false,
-//     "promotetoken": false,
-//     "demotetoken": false,
-//     "deletetoken": false,
-//     "stats": false
-// }
+client = userge_fed.Client(API_KEY)`;
 
-getcolor = {
-    "import": "#f92672",
-    "return": "#f92672",
-    "777000": "#ae81ff",
-    "=": "#f92672",
-    "id": "#f6aa11",
-    "token": "white",
-    'url': "#e6db74",
-    "https://api.userge.tk/version": "#e6db74",
-    "https://api.userge.tk/stats": "#e6db74",
-    'f"https://api.userge.tk/getban?api_key={': "#e6db74",
-    '}&user_id={': "#e6db74",
-    "}": "#e6db74",
-    "https://api.userge.tk/addban": "#e6db74",
-    "https://api.userge.tk/deleteban": "#e6db74",
-    "https://api.userge.tk/updateban": "#e6db74",
-    "https://api.userge.tk/createtoken": "#e6db74",
-    "https://api.userge.tk/promotetoken": "#e6db74",
-    "https://api.userge.tk/demotetoken": "#e6db74",
-    "https://api.userge.tk/deletetoken": "#e6db74"
+version_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.get_version)`;
+
+getban_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.get_ban(777000))`;
+
+getallban_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.get_ban())`;
+
+addban_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.add_ban(777000, "reason of ban"))`;
+
+updateban_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.update_ban(777000, "reason of ban"))`;
+
+deleteban_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.delete_ban(777000))`;
+
+get_me = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.get_me())`;
+
+create_token = `import requests
+
+url = "https://api.userge.tk/createtoken"
+
+data = {
+    "user_id": 777000,
+    "name": "Krishna"
 }
-// offsets = []
-// getPixelsforanimation()
 
-// function getPixelsforanimation() {
+return requests.post(url, data=data).json()`;
 
-//     width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-//     offsets = [3300, 4000, 4750, 5650, 6600, 7650, 8500, 9630, 10600, 11550]
-//     if (width > 1500)
-//          offsets = [2600, 3100, 3600, 4300, 4900, 5950, 6350, 7050, 7650, 8350]
-//     else if (width >= 1200)
-//          offsets = [2800, 3200, 3800, 4400, 5150, 5850, 6450, 7150, 7800, 8250]
-//     else if (width >= 900)
-//         offsets = [3150, 3550, 4050, 4650, 5400, 6120, 6700, 7500, 8150, 8800]
-//     else if (width >= 500)
-//         offsets = [3000, 3650, 4350, 5250, 6200, 7250, 8130, 9150, 10000, 11000]
+promote_token = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.promote_user(777000, ["can_ban", "can_unban"]))`;
+
+demote_token  = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.demote_user(777000))`;
+
+delete_token = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.delete_my_token()))`;
+
+stats_txt = `import userge_fed
+
+client = userge_fed.Client(API_KEY)
+
+print(client.get_api_stats()))`;
+
+carouselcheck = {
+    "version": false,
+    "getban": false,
+    "getallbans": false,
+    "addban": false,
+    "updateban": false,
+    "deleteban": false,
+    "getme": false,
+    "create_token": false,
+    "promotetoken": false,
+    "demotetoken": false,
+    "deletetoken": false,
+    "stats": false
+}
+
+// getcolor = {
+//     "import": "#f92672",
+//     "return": "#f92672",
+//     "777000": "#ae81ff",
+//     "=": "#f92672",
+//     "id": "#f6aa11",
+//     "token": "white",
+//     'url': "#e6db74",
+//     "https://api.userge.tk/version": "#e6db74",
+//     "https://api.userge.tk/stats": "#e6db74",
+//     'f"https://api.userge.tk/getban?api_key={': "#e6db74",
+//     '}&user_id={': "#e6db74",
+//     "}": "#e6db74",
+//     "https://api.userge.tk/addban": "#e6db74",
+//     "https://api.userge.tk/deleteban": "#e6db74",
+//     "https://api.userge.tk/updateban": "#e6db74",
+//     "https://api.userge.tk/createtoken": "#e6db74",
+//     "https://api.userge.tk/promotetoken": "#e6db74",
+//     "https://api.userge.tk/demotetoken": "#e6db74",
+//     "https://api.userge.tk/deletetoken": "#e6db74"
 // }
+offsets = []
+getPixelsforanimation()
 
-carousel(version_txt, "#version-txt")
-runAll()
+function getPixelsforanimation() {
+
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    offsets = [3030, 3650, 4330, 5250, 6100, 7000, 7870, 8400, 9350, 10250, 10950, 11500]
+    if (width > 1500)
+         offsets = [2600, 3100, 3550, 4200, 4900, 5600, 6200, 6450, 7250, 7900, 8500, 9000]
+    else if (width >= 1200)
+         offsets = [2450, 2900, 3430, 4050, 4750, 5500, 6050, 6330, 7150, 7800, 8400, 8750]
+    else if (width >= 900)
+        offsets = [2880, 3350, 3930, 4620, 5330, 6070, 6650, 7000, 7777, 8410, 9050, 9400]
+    else if (width >= 500)
+        offsets = [2700, 3350, 4000, 4950, 5850, 6650, 7440, 8020, 8960, 9800, 10520, 11020]
+}
+
+carousel(auth_text, "#authentication-txt")
+// runAll()
 
 
 // var $window = $(window), previousScrollTop = 0, scrollLock = false;
@@ -138,64 +218,91 @@ runAll()
 
 // });
 
-// window.addEventListener("scroll", function () {
-//     if(scrollLock) {
-//         $window.scrollTop(previousScrollTop); 
-//     }
+processing = false
 
-//     previousScrollTop = $window.scrollTop();
-//     if (window.pageYOffset > offsets[0]) {
-//         if (carouselcheck.getban == false) {
-//             carousel(getban_txt, "#getban-txt")
-//             carouselcheck.getban = true
-//         }
-//     } if (window.pageYOffset > offsets[1]) {
-//         if (carouselcheck.getallbans == false) {
-//             carousel(getallban_txt, "#getallbans-txt")
-//             carouselcheck.getallbans = true
-//         }
-//     } if (window.pageYOffset > offsets[2]) {
-//         if (carouselcheck.addban == false) {
-//             carousel(addban_txt, "#addban-txt")
-//             carouselcheck.addban = true
-//         }
-//     } if (window.pageYOffset > offsets[3]) {
-//         if (carouselcheck.updateban == false) {
-//             carousel(updateban_txt, "#updateban-txt")
-//             carouselcheck.updateban = true
-//         }
-//     } if (window.pageYOffset > offsets[4]) {
-//         if (carouselcheck.deleteban == false) {
-//             carousel(deleteban_txt, "#deleteban-txt")
-//             carouselcheck.deleteban = true
-//         }
-//     } if (window.pageYOffset > offsets[5]) {
-//         if (carouselcheck.createtoken == false) {
-//             carousel(create_token, "#create-token-txt")
-//             carouselcheck.createtoken = true
-//         }
-//     } if (window.pageYOffset > offsets[6]) {
-//         if (carouselcheck.promotetoken == false) {
-//             carousel(promote_token, "#promote-token-txt")
-//             carouselcheck.promotetoken = true
-//         }
-//     } if (window.pageYOffset > offsets[7]) {
-//         if (carouselcheck.demotetoken == false) {
-//             carousel(demote_token, "#demote-token-txt")
-//             carouselcheck.demotetoken = true
-//         }
-//     } if (window.pageYOffset > offsets[8]) {
-//         if (carouselcheck.deletetoken == false) {
-//             carousel(delete_token, "#delete-token-txt")
-//             carouselcheck.deletetoken = true
-//         }
-//     } if (window.pageYOffset > offsets[9]) {
-//         if (carouselcheck.stats == false) {
-//             carousel(stats_txt, "#stats-txt")
-//             carouselcheck.stats = true
-//         }
-//     }
-// })
+window.addEventListener("scroll", function () {
+    // if(scrollLock) {
+    //     $window.scrollTop(previousScrollTop); 
+    // }
+
+    // previousScrollTop = $window.scrollTop();
+    document.getElementById("demo").innerHTML = window.pageYOffset
+    if (processing == false) {
+        if (window.pageYOffset > offsets[0]) {
+            if (carouselcheck.getban == false) {
+                processing = true
+                carousel(getban_txt, "#getban-txt")
+                carouselcheck.getban = true
+            }
+        } if (window.pageYOffset > offsets[1]) {
+            if (carouselcheck.getallbans == false) {
+                processing = true
+                carousel(getallban_txt, "#getallbans-txt")
+                carouselcheck.getallbans = true
+            }
+        } if (window.pageYOffset > offsets[2]) {
+            if (carouselcheck.addban == false) {
+                processing = true
+                carousel(addban_txt, "#addban-txt")
+                carouselcheck.addban = true
+            }
+        } if (window.pageYOffset > offsets[3]) {
+            if (carouselcheck.updateban == false) {
+                processing = true
+                carousel(updateban_txt, "#updateban-txt")
+                carouselcheck.updateban = true
+            }
+        } if (window.pageYOffset > offsets[4]) {
+            if (carouselcheck.deleteban == false) {
+                processing = true
+                carousel(deleteban_txt, "#deleteban-txt")
+                carouselcheck.deleteban = true
+            }
+        } if (window.pageYOffset > offsets[5]) {
+            if (carouselcheck.create_token == false) {
+                processing = true
+                carousel(create_token, "#create-token-txt")
+                carouselcheck.create_token = true
+            } 
+        } if (window.pageYOffset > offsets[6]) {
+            if (carouselcheck.getme == false) {
+                processing = true
+                carousel(get_me, "#getme-txt")
+                carouselcheck.getme = true
+            }
+        } if (window.pageYOffset > offsets[7]) {
+            if (carouselcheck.promotetoken == false) {
+                processing = true
+                carousel(promote_token, "#promote-token-txt")
+                carouselcheck.promotetoken = true
+            }
+        } if (window.pageYOffset > offsets[8]) {
+            if (carouselcheck.demotetoken == false) {
+                processing = true
+                carousel(demote_token, "#demote-token-txt")
+                carouselcheck.demotetoken = true
+            }
+        } if (window.pageYOffset > offsets[9]) {
+            if (carouselcheck.deletetoken == false) {
+                processing = true
+                carousel(delete_token, "#delete-token-txt")
+                carouselcheck.deletetoken = true
+            }
+        }  if (window.pageYOffset > offsets[10]) {
+            if (carouselcheck.version == false) {
+                processing = true
+                carousel(version_txt, "#version-txt")
+                carouselcheck.version = true
+            }
+        } if (window.pageYOffset > offsets[11]) {
+            if (carouselcheck.stats == false) {
+                processing = true
+                carousel(stats_txt, "#stats-txt")
+                carouselcheck.stats = true
+            }
+        }
+    }
+})
 async function runAll() {
     await waitForMs(2000)
     carousel(getban_txt, "#getban-txt")
@@ -220,12 +327,11 @@ async function runAll() {
 }
 
 async function carousel(txt, eleRef) {
-    await typeInColor(txt, eleRef);
+    await typeSentence(txt, eleRef);
 }
 
 async function typeInColor(txt, eleRef) {
     let i = 0;
-    scrollLock = true;
     while(i < txt.length) {
         color = "white"
     if (txt[i] in getcolor)
@@ -233,18 +339,19 @@ async function typeInColor(txt, eleRef) {
     await typeSentence(txt[i], color, eleRef)
     i++;
     }
-    scrollLock = false;
+    // scrollLock = false;
     return;
 }
 
-async function typeSentence(sentence, color, eleRef, delay = 10) {
+async function typeSentence(sentence, eleRef, delay = 10) {
   let i = 0;
   letters = sentence.split("")
   while(i < letters.length) {
     await waitForMs(delay);
-    $(eleRef).append(letters[i].fontcolor(color));
+    $(eleRef).append(letters[i]);
     i++
   }
+  processing = false
   return;
 }
 
